2d57845f475ff259dcaba090efc77421a44e4428
Nathan Dinsmore <nfdins@gmail.com>
2014-11-07 20:03:11 -0500

    Formatting fixes

--- Introduction.elm
+++ Introduction.elm
@@ -34,7 +34,7 @@ Why do we need parentheses?
 
     main = asText 12 * 13
 
-This could mean `main = (asText 12) * 13` or `main = asText (12 * 13)`. Elm always chooses the first one, so we have to tell it to use the second one because we want to multiply the numbers *and then* convert the *output* (whatever $`12 \times 13`$ is) to an Element. It doesn't make much sense the other way around (what do you get when you multiply 13 by 10-point black Courier New text in the top-left corner of the screen?), which is why Elm will complain — rather noisily — if we do it that way or leave the parentheses off entirely:
+This could mean `main = (asText 12) * 13` or `main = asText (12 * 13)`. Elm always chooses the first one, so we have to tell it to use the second one because we want to multiply the numbers *and then* convert the *output* (whatever $`12 \times 13`$ is) to an Element. It doesn't make much sense the other way around (what do you get when you multiply $`13`$ by 10-point black Courier New text in the top-left corner of the screen?), which is why Elm will complain—rather noisily—if we do it that way or leave the parentheses off entirely:
 
 <div class=output>
 
@@ -46,7 +46,7 @@ This could mean `main = (asText 12) * 13` or `main = asText (12 * 13)`. Elm alwa
 
 </div>
 
-Makes sense: Elm wants a number to multiply by 13, but we're giving it an Element.
+Makes sense: Elm wants a number to multiply by $`13`$, but we're giving it an Element.
 
 Lists
 -----
@@ -70,7 +70,7 @@ What does this mean?
 
     main = asText [f 1, f 2, f 3, f 4, f 5, f 6]
 
-We can use our new `f` function like we used `asText` before — just `f` and then a space and then an input, like `2` or `5`.
+We can use our new `f` function like we used `asText` before—just `f` and then a space and then an input, like `2` or `5`.
 
 Higher-order functions
 ----------------------
@@ -132,7 +132,7 @@ Up until now, we've been telling Elm that `main` is an Element. Elements don't c
 
 |], timeline1 w, md [markdown|
 
-That's not the same representation of the time that we usually see — it's just a big number that keeps increasing — but let's use it to try out these signal things.
+That's not the same representation of the time that we usually see—it's just a big number that keeps increasing—but let's use it to try out these signal things.
 
 `(every second)` is a signal of *numbers*, though. We have to convert it to a signal of Elements. We could try using `asText` on it:
 
@@ -142,7 +142,7 @@ But this gives us:
 
 |], output w <| every second, md [markdown|
 
-Not very helpful. `asText` takes anything and converts it to an Element — not a *signal of* Elements. Since the values in the signal can change over time, the only thing that stays the same is that it's a signal — so that's what `asText` tells us. What we really want is to use `asText` on each of the *values* of `(every second)` instead the signal itself, like this:
+Not very helpful. `asText` takes anything and converts it to an Element—not a *signal of* Elements. Since the values in the signal can change over time, the only thing that stays the same is that it's a signal—so that's what `asText` tells us. What we really want is to use `asText` on each of the *values* of `(every second)` instead the signal itself, like this:
 
 |], timeline2 w, md [markdown|
 
@@ -158,14 +158,14 @@ Luckily, Elm has a function that does exactly this.
 diagram1 = diagram
   [ ("main ",   [markdown| `main` tells Elm what to show on the screen. |])
   , ("= ",      [markdown| `=` tells Elm that `main` is the same thing as the stuff on the right side. We call this process *defining* `main`, so the stuff after `=` is the *definition* of `main`. |])
-  , ("asText ", [markdown| `asText` turns something (like a number) into something else (an Element) that can show up on the screen. It's a function, which means it converts an input to an output. In this case, `100` is the input, and what you see on the screen is the output. Note that what you see on the screen isn't just a number — it's a number in black, 10-point Courier New at the top-left corner of the screen. This is the kind of information that Elements store but numbers do not. <p>When we want to use a function, we write the function and then its input, separated by a space. This is called *applying* the function. |])
+  , ("asText ", [markdown| `asText` turns something (like a number) into something else (an Element) that can show up on the screen. It's a function, which means it converts an input to an output. In this case, `100` is the input, and what you see on the screen is the output. Note that what you see on the screen isn't just a number—it's a number in black, 10-point Courier New at the top-left corner of the screen. This is the kind of information that Elements store but numbers do not. <p>When we want to use a function, we write the function and then its input, separated by a space. This is called *applying* the function. |])
   , ("12 ",     [markdown| `100` is just a number. When we write a number the way we normally would, Elm gives us a number. |])
   ]
 
 diagram2 = diagram
   [ ("f ",             [markdown| `f` is the name of the function. We need a name (like `asText`) so that we can use it later. |])
   , ("x ",             [markdown| `x` is the input to `f`. Or, more accurately, it's a name we use for the input to `f`. Whenever we use the function, Elm replaces every `x` in the definition of `f` (remember, that's the stuff after the `=` sign) with the input we give it, like `12` or `5`. |])
-  , ("= x * (x + 1) ", Math.markdown [markdown| `x * (x + 1)` is more of the stuff we've seen before, except now it has an `x` in it. We need parentheses around `x + 1` because Elm goes by the same order of operations we do — multiplication and division, then addition and subtraction. We don't want $`x^2 + 1`$; we want $`x \times (x + 1)`$. |])
+  , ("= x * (x + 1) ", Math.markdown [markdown| `x * (x + 1)` is more of the stuff we've seen before, except now it has an `x` in it. We need parentheses around `x + 1` because Elm goes by the same order of operations we do—multiplication and division, then addition and subtraction. We don't want $`x^2 + 1`$; we want $`x \times (x + 1)`$. |])
   ]
 
 timeline1 = timeline <| map (\t -> (1, centered << toText << show <| 173081 + t * 1001)) [1 .. 4]
